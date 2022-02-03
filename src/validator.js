8cb668b55f474aa6a44bded2f860b0a23be86d51
Fe-Monteiro <fe.199nanda@gmail.com>
2022-02-03 20:01:50 -0300

    Código Funcionando

--- src/validator.js
+++ src/validator.js
@@ -6,40 +6,24 @@ const validator = {
       return 1;
     }
 
-    let nCheck = 0; //Var onde vou somar todos os campos para verificação
-    let skipDigit = false; //Var onde vou modificar o liga e desliga para pular as casas no cardNum
+    let nCheck = 0;
+    let skipDigit = false;
 
     for (let nPosition = cardNumberValue.length - 1; nPosition >= 0; nPosition--) {
-      let cDigit = cardNumberValue.charAt(nPosition); //Var que informa o valor na posição atual
-      let nDigit = parseInt(cDigit, 10); /*Var que garante que o número da posição atual esteja inteiro na base decimal*/
+      let cDigit = cardNumberValue.charAt(nPosition);
+      let nDigit = parseInt(cDigit, 10);
 
-      if (skipDigit) { //Entra na função If somente se o skipDigit for igual a true
-        if ((nDigit *= 2) > 9) nDigit -= 9; /*Se o skipDigit for true, ele vai pegar a posição atual
-            que está sendo informada na var nDigit, vai multiplicar por 2 e atribuir ao nDigit esse
-            valor multiplicado. Caso o novo valor atribuido ao nDigit seja maior do que 9, ele vai
-            pegar o novo valor do nDigit e subtrair 9, o resultado dessa subtração, será o novo valor
-            do nDigit*/
-        // }else if(nDigit > 9) {
-        //   nDigit -= 9  /* Aqui o nDigit já está multiplicado e se for maior do que 9 Caso o novo valor atribuido ao nDigit seja maior do que 9, ele vai
-        //     pegar o novo valor do nDigit e subtrair 9, o resultado dessa subtração, será o novo valor
-        //     do nDigit*/
-        // } else {
-        // nDigit = nDigit.value;
+      if (skipDigit) {
+        if ((nDigit *= 2) > 9) nDigit -= 9;
       }
       
-      nCheck += nDigit; /*O nCheck é a variavel onde estamos somando o valor de todos os digitos (nDigit)
-              //  após a tratativa de somar digito sim e digito não que está na condição if (skipDigit). Dessa
-              //  forma, a cada vez que a condição For for executada, será somado no nCheck o valor do atual nDigit*/
+      nCheck += nDigit;
 
-
-
-      skipDigit = !skipDigit; /*É onde troca a "chave" de true e false da variavel skipDigit. Essa "chave"
-             é o que faz ele identificar qual o número que irá entrar na condição If, ou seja, que irá
-             multiplicar ou não.*/
+      skipDigit = !skipDigit;
 
     }
 
-      if (nCheck % 10 == 0/*= 0 && cardNumberValue !== "" && nCheck !== 0*/) {
+      if (nCheck % 10 === 0 && cardNumberValue !== "" && nCheck !== 0) {
         return true;
       } else {
         return false;
